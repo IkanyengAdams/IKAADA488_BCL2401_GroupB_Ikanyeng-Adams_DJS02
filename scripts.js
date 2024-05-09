10631e3d7632d1d26ff93182375b043bcf6b8969
Ikanyeng Adams <ikanyengadams2@gmail.com>
2024-05-09 11:57:32 +0200

    created two function for validation and anything that is not a number

--- scripts.js
+++ scripts.js
@@ -6,4 +6,19 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = dividend / divider;
+
+  //validation when values are missing
+if (!dividend || !divider) {
+  result.innerText = "Division not performed. Both values are required in inputs. Try again"; 
+  return;
+}
+
+//Providing anything that is not a number should crash the program
+if (isNaN(dividend) || isNaN(divider)) {
+  document.body.innerHTML = "Something critical went wrong. PLease reload the page";
+  console.error("Invalid input provided");
+  return
+}
+
 });
+
